bed1e866a822461bd8e534734786b528bdac3620
Grant Slape <notrilia.republic@gmail.com>
2018-01-06 12:57:50 -0600

    remove unneeded casks

--- setup-scripts/cask.sh
+++ setup-scripts/cask.sh
@@ -3,20 +3,16 @@ brew tap phinze/homebrew-cask
 brew install brew-cask
 
 # Core / Personal
-brew cask install 1password
 brew cask install alfred
-brew cask install dropbox
 brew cask install google-chrome
 brew cask install istat-menus
 brew cask install nosleep
-brew cask install mou
 brew cask install slack
 brew cask install spotify
 brew cask install the-unarchiver
 brew cask install vlc
 
 # Development
-brew cask install atom
 brew cask install dash
 brew cask install github-desktop
 brew cask install iterm2
